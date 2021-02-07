5defe125c750bf415444af9337f0bd256902814e
Molly <mollymccollumwx@gmail.com>
2021-02-07 15:39:37 -0500

    added virtuals:true to make mongoose attach virtuals whenever calling res.json

--- models/Workout.js
+++ models/Workout.js
@@ -1,38 +1,45 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-
-  exercises: [
-    {
-      type: {
-        type: String,
-      },
-      name: {
-        type: String,
-      },
-      duration: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-      distance: {
-        type: Number,
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-});
+
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
