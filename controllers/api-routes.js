7b1df1e86455fd47764ea064055329a72273198c
Molly <mollymccollumwx@gmail.com>
2021-02-07 14:54:33 -0500

    created post route for api/workouts

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,9 +1,18 @@
+//require dependencies 
 const express = require("express");
 const Router = express.Router();
 const db = require("../models");
 
+// GET route for all workouts
 Router.get("/api/workouts", (req, res) => {
   db.Workout.find().then((allWorkouts) => {
     res.json(allWorkouts);
+  });
+});
+
+// POST route for a new workout
+Router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body).then((newWorkout) => {
+    res.json(newWorkout);
   });
 });
