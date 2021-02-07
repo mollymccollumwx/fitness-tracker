1c19353c81fa6f435ced7b37168507be2cfcff67
Molly <mollymccollumwx@gmail.com>
2021-02-07 15:00:59 -0500

    added the get route for viewing the add exercise page, tested it and it works

--- controllers/views.js
+++ controllers/views.js
@@ -3,6 +3,11 @@ const express = require("express");
 const path = require("path");
 const Router = express.Router();
 
+// viewing the add exercise page
+Router.get("/exercise", (req, res)=> {
+    res.sendFile(path.join(__dirname, "../public/exercise/html"));
+})
+
 
 
 
