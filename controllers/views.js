e09010f2796e85d87de2e57f1be295f4def5b9b0
Molly <mollymccollumwx@gmail.com>
2021-02-07 15:03:09 -0500

    added comments to the view.js file

--- controllers/views.js
+++ controllers/views.js
@@ -3,11 +3,12 @@ const express = require("express");
 const path = require("path");
 const Router = express.Router();
 
-// viewing the add exercise page
+// view the add exercise page
 Router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+// view for the stats page
 Router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
