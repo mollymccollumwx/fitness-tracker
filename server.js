efbdb621fb2253231375c2e90e0a69e4e11cc2b3
Molly <mollymccollumwx@gmail.com>
2021-02-07 14:27:30 -0500

    added static express to server up the public folder

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ connection.on("error", (err) => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
