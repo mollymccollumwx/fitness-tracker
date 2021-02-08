41a3a0e9ac5bfbd2b17224344adaa9f70258c9d3
Molly <mollymccollumwx@gmail.com>
2021-02-08 09:35:39 -0500

    fixed so that seed data is running now. Changed the connection in the seed.js to run on Heroku as well

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,15 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+const Workout = require("../models/Workout");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
 
 let workoutSeed = [
   {
@@ -124,8 +129,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
